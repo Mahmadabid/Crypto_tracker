fd8496db5da0d6eea49fba14ab50a0f3a8ec85d5
Muhammad Ahmad <75790323+Mahmadabid@users.noreply.github.com>
2023-07-10 11:30:15 +0000

    Cre

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const App = () => {
+  const headerStyle = {
+      background: 'linear-gradient(to right, black, purple)',
+          color: 'white',
+            };
+
+              const mainStyle = {
+                  background: 'linear-gradient(to right, black, purple)',
+                      color: 'white',
+                        };
+
+                          return (
+                              <div>
+                                    <header style={headerStyle}>
+                                            <h1>Crypto Tracker</h1>
+                                                  </header>
+                                                        <main style={mainStyle}>
+                                                                {/* Content for the main section of the page */}
+                                                                      </main>
+                                                                            <footer>
+                                                                                    {/* Content for the footer section of the page */}
+                                                                                          </footer>
+                                                                                              </div>
+                                                                                                );
+                                                                                                };
+
+                                                                                                export default App;
+                                                                                                
